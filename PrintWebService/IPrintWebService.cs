97d9b5e006576d32f02f2d46084480927bef2678
NathanG-HEG <92545039+NathanG-HEG@users.noreply.github.com>
2022-04-08 09:43:06 +0200

    All lot of things

---
+++ PrintWebService/IPrintWebService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using DTO;
+
+namespace PrintWebService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IPrintWebService" in both code and config file together.
+    [ServiceContract]
+    public interface IPrintWebService
+    {
+        [OperationContract]
+        bool IsRegisterd(string userName);
+
+        [OperationContract]
+        bool Print(int nbCopies, string productName, int cardId);
+
+        [OperationContract]
+        void TransferMoney(int userId, float quota);
+
+        [OperationContract]
+        void TransferMoney(string userName, float quota);
+
+        [OperationContract]
+        User GetUserByCardId(int cardId);
+
+        [OperationContract]
+        void DebitAccount(int userId);
+        
+        
+
+    }
+}
---
+++ PrintWebService/PrintWebService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace PrintWebService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "PrintWebService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select PrintWebService.svc or PrintWebService.svc.cs at the Solution Explorer and start debugging.
+    public class PrintWebService : IPrintWebService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
