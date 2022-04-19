4875bcda73338928d592ad9deb6ac8cb885a3f8e
NathanG-HEG <92545039+NathanG-HEG@users.noreply.github.com>
2022-04-19 20:43:33 +0200

    Benjamin cant access a folder (4e semestre hes)

---
+++ BLL/managers/PrintPriceManager.cs
@@ -0,0 +1,21 @@
+﻿using BLL.interfaces;
+using DAL;
+
+namespace BLL.managers
+{
+    public class PrintPriceManager:IPrintPriceManager
+    {
+        private IPrintPriceDBA printPriceDba;
+
+        public PrintPriceManager(IPrintPriceDBA printPriceDba)
+        {
+            this.printPriceDba = printPriceDba;
+        }
+
+        public float GetPriceByProductName(string productName)
+        {
+            return printPriceDba.GetPriceByProductName(productName);
+        }
+
+    }
+}
