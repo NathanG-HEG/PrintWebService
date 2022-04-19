bca30bea5edf477b5dfce94f9cf2666ad6531477
NathanG-HEG <92545039+NathanG-HEG@users.noreply.github.com>
2022-04-19 21:00:27 +0200

    12345678

---
+++ BLL/managers/PrintSystemManager.cs
@@ -0,0 +1,21 @@
+﻿using BLL.interfaces;
+using DTO;
+
+namespace BLL.managers
+{
+    public class PrintSystemManager:IPrintSystemManager
+    {
+        private IPrintSystemManager printSystemManager;
+
+        public PrintSystemManager(IPrintSystemManager printSystemManager)
+        {
+            this.printSystemManager = printSystemManager;
+        }
+
+
+        public User GetUserByCardId(int cardId)
+        {
+            return printSystemManager.GetUserByCardId(cardId);
+        }
+    }
+}
---
+++ BLL/managers/SapManager.cs
@@ -0,0 +1,26 @@
+﻿using BLL.interfaces;
+using DAL;
+using DTO;
+
+namespace BLL.managers
+{
+    public class SapManager:ISapManager
+    {
+        private ISAP_DBA sapDba;
+
+        public SapManager(ISAP_DBA sapDba)
+        {
+            this.sapDba = sapDba;
+        }
+
+        public User GetUserByUsername(string username)
+        {
+            return sapDba.GetUserByUsername(username);
+        }
+
+        public User GetUserByCardId(int cardId)
+        {
+            return sapDba.GetUserByCardId(cardId);
+        }
+    }
+}
