28e37300975b67636de264725ec3df8566815681
benjamin <benjamin.biollaz@students.hevs.ch>
2022-04-20 18:15:34 +0200

    console app test

---
+++ Console_app_test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using DTO;
+
+namespace Console_app_test
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintWebService.PrintWebServiceClient printWs = new PrintWebService.PrintWebServiceClient();
+            User user =  printWs.GetUserByCardId(123456789);
+            Console.WriteLine(user.Balance);
+            Console.WriteLine("hello world");
+        }
+    }
+}
